b26c645ba090a2fe7fb320d3866ab15aea5443ff
EnzoRoche-STUDENT <EnzoRoche-STUDENT@S-1N1H504-SL08>
2026-05-21 13:02:22 +0100

    Add project files.

---
+++ ExamEnzoRocheS00295665/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ExamEnzoRocheS00295665
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
